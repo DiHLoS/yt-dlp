643a0852b5d9297ea1ab696923065f038f5a5032
Sergii <37122982+DiHLoS@users.noreply.github.com>
2021-10-18 16:38:40 +0300

    Update utils.py

--- yt_dlp/utils.py
+++ yt_dlp/utils.py
@@ -1677,7 +1677,7 @@ def random_user_agent():
 
 
 std_headers = {
-    'User-Agent': random_user_agent(),
+    #'User-Agent': random_user_agent(),
     'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
